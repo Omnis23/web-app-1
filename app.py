3724e9c3661da3327af9b968ece9f8462a1a319f
Sami <samijo0323@outlook.com>
2023-12-21 12:42:00 -0600

    Pushing up baseline modifications/work on current web app

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
+'''
+Created by: omnis23
+Date started: 12/15/2023
+
+This is a way for me to practice Python Flask, HTML, CSS and
+JavaScript by doing my own versin of a website refresh for a
+business in my hometown.
+'''
+
+from datetime import datetime
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
